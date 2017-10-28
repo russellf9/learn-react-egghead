a44eac72b73cf91a1790df9a9b8560ad1e4a9916
Russell Wenban <factornine@MacBook-Pro.local>
2017-10-28 14:44:55 +0100

    Egghead - React - Lesson 14 - Control React Component Updates When New Props Are Received - v2

--- src/App.js
+++ src/App.js
@@ -7,26 +7,35 @@ class App extends React.Component {
         this.state = {items: []}
 
     }
-
     componentWillMount() {
         fetch('https://swapi.co/api/people/?format=json')
             .then(response => response.json())
             .then(({results: items}) => this.setState({items}))
     }
-
-
+    filter(e) {
+        this.setState({filter: e.target.value})
+    }
     render() {
         let items = this.state.items;
+        if (this.state.filter) {
+            items = items.filter(item =>
+            item.name.toLowerCase()
+                .includes(this.state.filter.toLowerCase()))
+        }
         return (
             <div>
-                { items.map(item => <h4 key={item.name}>{item.name}</h4>)}
+                <input type="text"
+                       onChange={this.filter.bind(this)} />
+                { items.map(item =>
+                    <Person key={item.name} person={item}/>)
+                }
             </div>
         )
-
     }
 }
 
-export default App
+const Person = (props) => <h4>{props.person.name}</h4>;
 
+export default App
 
 
